c82857abacc9a08350d0a565f594117ba084b21d
rajatdhiman278 <rajatdhiman115@gmail.com>
2022-09-22 08:09:53 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
